065eb31d67f87e80757edcf410910c3db2c03bcf
OpesMentis <planchot.antoine@gmail.com>
2016-08-23 17:44:02 +0200

    Additional tests in case of missing input.

--- main.cpp
+++ main.cpp
@@ -5,12 +5,23 @@
 #include "generator.h"
 
 int main (int argc, char ** argv) {
+	if (argc < 3) {
+		printf("Missing file(s) in input.\n");
+		printf("Usage: './Convertor file.c file.mxif'.\n");
+		exit(0);
+	}
+
 	FILE * f_mxif = fopen(argv[2], "r");
 	if (f_mxif == NULL) {
+		printf("Failure in the openning of the MXIF file.\n");
 		exit(0);
 	}
 	argv[2] = NULL;
-	SgProject * project = frontend(2, argv);
+	SgProject *project = frontend(2, argv);
+	if (project == NULL) {
+		printf("Failure in the openning of the C file.\n");
+		exit(0);
+	}
 	generateCode(argc, argv, project, f_mxif);
 	fclose(f_mxif);
 	
